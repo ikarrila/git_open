291f4e28a42b87bebf219cb13948bc69d8dbb8e4
ikarrila <mjunposti@gmail.com>
2023-05-10 21:00:53 +0300

    Merge branch 'feature/add-logging'

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 print("Hello world!")
-<<<<<<< HEAD
-=======
-print("Hello world!")
->>>>>>> intermediate_normal/master
-=======
 print("Hello once again")
->>>>>>> intermediate_normal/feature/add-logging
